1ca84673c838a88f3858cc3a9d62b1f693ab0894
laravitoria44 <laravitoriaalmeida6@gmail.com>
2026-08-11 11:02:39 -0300

    .

--- obras/models.py
+++ obras/models.py
@@ -5,7 +5,7 @@ class Obra(models.Model):
     titulo = models.CharField(max_length=150)
     tipo = models.CharField(max_length=100)
     resumo = models.TextField()
-    capa = models.ImageField(upload_to='capas/', blank=True, null=True)
+    capa = models.ImageField(upload_to='capas/')
     temas = models.ManyToManyField(TemaEnem, related_name='obras')
 
     def __str__(self):
--- temas/models.py
+++ temas/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class TemaEnem(models.Model):
     nome = models.CharField(max_length=150)
+    models.ImageField(upload_to='capas/')
+
 
     def __str__(self):
         return self.nome
