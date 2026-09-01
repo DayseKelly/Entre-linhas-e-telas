4f98ec0a5eb588ec742e1f0b739c429fa8c16331
laravitoria44 <laravitoriaalmeida6@gmail.com>
2026-09-01 14:02:27 -0300

    continuação das views

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('obras/', include('obras.urls')),
+    path('escolas/', include('escolas.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    path('avaliacoes/', include('avaliacoes.urls')),
+    path('usuario-obra/', include('usuario_obra.urls')),
 ]
--- usuario_obra/views.py
+++ usuario_obra/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UsuarioObra
+from .forms import UsuarioObraForm
 
-# Create your views here.
+def listar_usuario_obras(request):
+    registros = UsuarioObra.objects.all()
+    return render(request, 'usuario_obra/lista_usuario_obras.html', {'registros': registros})
+
+def criar_usuario_obra(request):
+    if request.method == 'POST':
+        form = UsuarioObraForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_usuario_obras')
+    else:
+        form = UsuarioObraForm()
+    return render(request, 'usuario_obra/form_usuario_obra.html', {'form': form})
