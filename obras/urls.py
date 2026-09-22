173d17e24ece8504e39b19cacfffee7cc26de516
laravitoria44 <laravitoriaalmeida6@gmail.com>
2026-09-22 04:31:52 -0300

    detalhes

--- obras/urls.py
+++ obras/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.listar_obras, name='listar_obras'),
     path('criar/', views.criar_obra, name='criar_obra'),
+    
+    # <int:id> significa que a URL espera um número inteiro (ex: /obras/1/, /obras/2/)
+    path('<int:id>/', views.detalhe_obra, name='detalhe_obra'),
 ]
--- obras/views.py
+++ obras/views.py
@@ -15,3 +15,10 @@ def criar_obra(request):
  else:
   form = ObraForm()
  return render(request, 'obras/form_obra.html', {'form': form})
+
+ def detalhe_obra(request, id):
+    # O get_object_or_404 busca a obra pelo ID no banco de dados.
+    # Se o ID não existir (ex: /obras/9999/), ele abre uma página de erro 404 em vez de quebrar o site.
+    obra = get_object_or_404(Obra, id=id)
+    
+    return render(request, 'obras/detalhe_obra.html', {'obra': obra})
